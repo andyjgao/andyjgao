beba7a64b24664ee14a22d59058c96070e74fe9b
Andy Gao <agao@andrew.cmu.edu>
2020-03-28 20:02:21 -0400

    rebuilding site Sat Mar 28 20:02:21 EDT 2020

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Go To Public folder
+cd themes/hugo-theme-chunky-poster
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+git submodule update --rebase --remote
+cd ../..
+
+git add . 
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+git push origin master
